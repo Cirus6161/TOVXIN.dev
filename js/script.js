adbe7fe240879a7a3b12fba977a769dcf414cf6b
Unknown <cirus6161@gmail.com>
2018-03-29 13:46:58 +0300

    Очередная ебула

--- js/script.js
+++ js/script.js
@@ -307,5 +307,23 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function(){
+
+    $("#slow-down").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
 
 
